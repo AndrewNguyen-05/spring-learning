6c61f5d8d4b2ebb8a689537183269befbdffbd04
andrewnguyen-05 <21520144@gm.uit.edu.vn>
2024-02-12 11:42:07 +0700

    Add sql scripts

---
+++ 06-spring-MVC/ValidationDemo/src/test/java/com/luv2code/springboot/validationdemo/ValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.validationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
